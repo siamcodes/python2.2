25f139d41071ae4842f2420b166ca1be9295d157
siamcodes <siamcodes@gmail.com>
2024-06-26 11:29:46 +0700

    update

---
+++ lab4-3.py
@@ -0,0 +1,4 @@
+person = ["Somkiat", 35, 172, 74, "CDTI"]
+print(person)
+print(person[2:])
+#print(person[5])
---
+++ lab4-4.py
@@ -0,0 +1,12 @@
+p = {
+    'name': "Somkiat Jaidee",
+    "age": 35,
+    "w": 75,
+    'h':175,
+    "email": "eleclabs@gmail.com"
+    }
+
+print("สวัสดีครับคุณ %s" % p["name"])
+print("อายุ %d น้ำหนัก %d ส่วนสูง %d" % (p["age"], p["w"], p["h"]))
+print("อีเมล %s" % (p["email"]))
+
